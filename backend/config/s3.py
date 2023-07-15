832e62e0bb2778f8f2366bdffcb89314d1f772d7
Rehan <abusayid693@gmail.com>
2023-07-15 14:47:48 +0530

    fix: aws security issue

--- backend/config/s3.py
+++ backend/config/s3.py
@@ -1,7 +1,9 @@
+import os
+
 import boto3
 
-AWS_ID = "AKIA5CFV7QMLRDGBKT52"
-AWS_SECRET = "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"
-AWS_BUCKET="layer-training-data-bucket"
+AWS_ID = os.environ.get('AWS_ACCESS_KEY')
+AWS_SECRET = os.environ.get('AWS_SECRET')
+AWS_BUCKET= os.environ.get('AWS_S3_BUCKET')
 
 s3 = boto3.client("s3", aws_access_key_id=AWS_ID, aws_secret_access_key=AWS_SECRET)
