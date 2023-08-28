a75e18927d86fd75135b9f4994f218d5b0638f31
Rehan <abusayid693@gmail.com>
2023-08-29 03:26:01 +0530

    added jwt login token

--- backend/routes/user_route/helper.py
+++ backend/routes/user_route/helper.py
@@ -1,7 +1,12 @@
-from sqlalchemy.orm import Session
+import datetime
+import os
+
+import jwt
+from db_models.user.schema import LoginRequest, UserSchema
 from db_models.user.user import User
-from db_models.user.schema import UserSchema, LoginRequest
-from fastapi import  HTTPException
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+
 
 def create_user(db: Session, data: UserSchema):
     _user = User(name=data.name, email=data.email, password=data.password)
@@ -10,9 +15,19 @@ def create_user(db: Session, data: UserSchema):
     db.refresh(_user)
     return _user
 
-def login_user(db:Session, data:LoginRequest):
+
+def login_user(db: Session, data: LoginRequest):
     user = db.query(User).filter_by(email=data.email).first()
+
     if user and user.compare_password(data.password):
-        return {"token":"sdsdnsj"}
+        payload = {
+        "user_id": 123,
+        "exp": datetime.datetime.utcnow()
+        + datetime.timedelta(hours=100),  # Token expiration time
+        }
+        
+        token = jwt.encode(payload, os.environ.get('JWT_SECRET'), algorithm='HS256')
+        print("Token:", token)
+        return {"token": token}
     else:
         raise HTTPException(status_code=401, detail="Invalid crediantials")
