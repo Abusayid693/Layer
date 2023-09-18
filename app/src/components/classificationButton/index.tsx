76c731f0e743df51a12b29d359ed364acf81bddc
Rehan <abusayid693@gmail.com>
2023-09-19 03:37:53 +0530

    typo fix

--- app/src/components/classificationButton/index.tsx
+++ app/src/components/classificationButton/index.tsx
@@ -7,7 +7,7 @@ export const ClassificationButton = ({isImage}: any) => {
     <TouchableOpacity style={{width: '49%'}}>
       <S.Container style={{backgroundColor: isImage ? '#AD290C' : '#2A0F97'}}>
         <PlusIcon />
-        <S.ButtonText>Image</S.ButtonText>
+        <S.ButtonText>{isImage ? 'Image' :'CSV'}</S.ButtonText>
       </S.Container>
     </TouchableOpacity>
   );
