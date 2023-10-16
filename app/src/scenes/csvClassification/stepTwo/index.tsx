9e8871ea2346d59f8555ca96ea74a5821dfaa63d
Rehan <abusayid693@gmail.com>
2023-10-17 03:18:03 +0530

    layers input order

--- app/src/scenes/csvClassification/stepTwo/index.tsx
+++ app/src/scenes/csvClassification/stepTwo/index.tsx
@@ -1,11 +1,35 @@
-import { HiddenLayerInput } from "../../../components";
-import * as S from '../style';
+import { useState } from 'react';
+import { HiddenLayerInput } from '../../../components';
+import PlusIcon from '../assets/arrow';
+import * as S from './style';
 
-export const StepTwo = ({setIndex}:any) => {
+export const StepTwo = ({setIndex}: any) => {
+  const [layerCount, setLayerCount] = useState(2);
+
+  const [layers, setLayers] = useState({
+    layer1: '',
+  });
+
+  const handleAddLayers = () => {
+    setLayers(prev => ({
+      ...prev,
+      [`layer${layerCount}`]: '',
+    }));
+
+    setLayerCount(prev => prev + 1);
+  };
+
+  console.log('layers :', layers)
+
   return (
     <S.FormContainer>
-      <S.FormLabel>Hidden Layers</S.FormLabel>
-        <HiddenLayerInput name={`layer`} />
+      <S.Header>
+        <S.FormLabel>Hidden Layers</S.FormLabel>
+        <S.AddButton onPress={handleAddLayers}>
+          <PlusIcon />
+        </S.AddButton>
+      </S.Header>
+      <HiddenLayerInput name={`layer`} />
     </S.FormContainer>
   );
 };
--- app/src/scenes/csvClassification/stepTwo/style.ts
+++ app/src/scenes/csvClassification/stepTwo/style.ts
@@ -6,3 +6,17 @@ export const FormContainer = styled.View`
 background-color: #EEEEEF;
 padding: ${scaleSize(10)}px;
 `
+export const Header = styled.View`
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+padding-right: ${scaleSize(20)}px;
+`
+
+export const AddButton = styled.TouchableOpacity`
+
+`
+
+export const FormLabel = styled.Text`
+font-weight: 400;
+`
